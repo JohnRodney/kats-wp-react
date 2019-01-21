8b84e269cdb4b52d247273a3fd122b984b4f17e1
John Rodney <johnrodney84@gmail.com>
2019-01-21 17:55:16 -0600

    base conversion to kats begins

--- wp-theme/header.php
+++ wp-theme/header.php
@@ -2,5 +2,6 @@
   <head>
     <?php wp_head(); ?>
     <meta name="google-site-verification" content="aw7QJDXJ-_E_bhXRh7Sn0zUeutWGYHO22zoyYPyYk4Q" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
